c23e0b9c04d287276f4a1e596c4a09565003dcd9
Estefane <estefanecortes@gmail.com>
2021-10-27 11:24:27 -0300

    tentativa de criacao de rotas

--- index.js
+++ index.js
@@ -22,10 +22,45 @@ function createRoutes() {
     }
   ]
 
-  // const counter = 0;
-  // // while(counter < 3) {
-  // //   if(routes)
-  // // }
+  //precisam de entradas:
+  // norte, sudeste
+  //precisam de saidas:
+  //nordeste, centro-oeste, sul
+
+  const precisamEntradas = ["norte", "sudeste"];
+  const precisamSaidas = ["nordeste", "centro-oeste", "sul"]
+
+  let counter = 0;
+  for(let i=0; i<routes.length;i++) {
+    if(!routes[i].entrada.length) {
+      let j = 0;
+      if(precisamSaidas[0] === routes[i].saida[0]) {
+        j++;
+        routes[i].entrada.push(precisamSaidas[j]);
+      } else {
+        routes[i].entrada.push(precisamSaidas[j]);
+      }
+      precisamSaidas.splice(j, 1);
+      counter++;
+    }
+    if(!routes[i].saida.length) {
+      let j = 0;
+      if(precisamEntradas[0] === routes[i].entrada[0]) {
+        j++;
+        routes[i].saida.push(precisamEntradas[j]);
+      } else {
+        routes[i].saida.push(precisamEntradas[j]);
+      }
+      precisamEntradas.splice(j, 1);
+      counter++;
+    }
+  }
+
+  console.log(routes);
+  console.log(counter);
 }
 
 createRoutes();
+
+//pra eu sair de x regiao para y, por quantas rotas eu passo? 
+//
